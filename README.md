374d8a08259cc7b98c8acb2f0f2744178271fe89
AlvaroQuintero <adquinterop@ufpso.edu.co>
2024-10-05 07:53:01 -0500

    Desarrollo Ejercicio 1

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Parcial-1-Logica
+
+Ejercicio 01:
+
+creare una condicion para hacer un ciclo y que el programa se repita hasta que el usuario decida salir
+El codigo analiza si los numeros son primos y muestra cuantos numeros primos existen entre ellos haciendo uso del condicional if
+Al final muestra un texto que describe la entrada de datos, muestra la cantidad de numeros primos y los numeros primos
---
+++ contarNumerosPrimos.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class contarNumerosPrimos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        boolean opcion = true;
+
+        while (opcion) {
+            System.out.println("Ingrese dos números enteros positivos: ");
+            int numero1 = scanner.nextInt();
+            int numero2 = scanner.nextInt();
+            if () {
+                int primos1 = numero1 / 1;
+                int Primos1 = numero1 / numero1;
+                int primos2 = numero2 / 1;
+                int Primos2 = numero2 / numero2;
+        } 
+            
+
+            System.out.println("Entrada: " + numero1 + ", " + numero2 + "// " + "(" + primos1 + primos2 + ")");
+        
+        
+        }
+    }
+}
